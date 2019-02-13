fd8c43bf64605566e7aff2fe4f3efa91ed408e95
Lucas Reynolds <Lucas.Reynolds@hyland.com>
2019-02-12 19:17:10 -0500

    Created an about me program.

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -7,7 +7,43 @@ namespace Week02
         static void Main(string[] args)
         {
             // Create variables and do things with them!
-            
+            Console.WriteLine("Hello World");
+
+            //About me
+            string myName = "Josh";
+            int myAge = 32;
+            string favColor = "blue";
+            string myHobby = "guitar";
+            bool isOldEnoughToDrive = true;
+            double myHeight = 5.58;
+            char firstInitial = 'j';
+
+            Console.WriteLine("My name is " + myName);
+            //Console.WriteLine("My age is {0}", myAge);
+            Console.WriteLine("My age is " + myAge);
+            Console.WriteLine("My favorite color is " + favColor);
+            Console.WriteLine("My favorite hobby is " + myHobby);
+            Console.WriteLine("My height is " + myHeight + " feet tall.");
+            Console.WriteLine("The first initial of my name is " + firstInitial);
+            Console.WriteLine("I am old enough to drive: " + isOldEnoughToDrive);
+
+                        //About me
+            string A = "Josh";
+               int dog = 32;
+                string favCZolor = "blue";
+            string myHobby = "guitar";
+               bool blah = true;
+                                double d = 5.58;
+            char c = 'j';
+
+            Console.WriteLine("My name is " + A);
+            //Console.WriteLine("My age is {0}", myAge);
+                        Console.WriteLine("My age is " + dog);
+                                                                                        Console.WriteLine("My favorite color is " + favCZolor);
+        Console.WriteLine("My favorite hobby is " + myHobby);
+    Console.WriteLine("My height is " + d + " feet tall.");
+            Console.WriteLine("The first initial of my name is " + c);
+                    Console.WriteLine("I am old enough to drive: " + blah);
         }
     }
 }
