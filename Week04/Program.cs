0aff25bcbbc94a300e63ebb66704a6d34f2ff34c
Lucas Reynolds <Lucas.Reynolds@hyland.com>
2019-02-26 19:23:44 -0500

    Did week 4 code along!

---
+++ Week04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool snowing = true;
+            bool haveCarrot = true;
+            int temp = 40;
+            string clothes = "outfit";
+            string name = "Lucas";
+
+            if ((snowing && temp <= 40) && (name != "Bob" || haveCarrot)) {
+                Console.WriteLine("I'm a snowman!");
+            }
+        }
+    }
+}
