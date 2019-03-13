4537ac02d338a789a6d5174ed02fd28becdd47ea
Lucas Reynolds <Lucas.Reynolds@hyland.com>
2019-03-13 10:28:48 -0400

    Week 6 - Wrote factorial to practice while loops

---
+++ Week06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(userInput));
+        }
+
+        public static int factorial(int number)
+        {
+            int total = 1;
+            while (number > 0)
+            {
+                total *= number;
+                number--;
+            }
+            return total;
+        }
+    }
+}
